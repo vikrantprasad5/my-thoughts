cd7d1e3797c61eb62bd523f5868c28cd7a99a517
vikrantprasad5 <vikrantprasad5@gmail.com>
2020-09-04 18:39:54 +0530

    add procfile

--- app.js
+++ app.js
@@ -104,8 +104,13 @@ const requestedPostId = req.params.postId;
 
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started ");
 });
